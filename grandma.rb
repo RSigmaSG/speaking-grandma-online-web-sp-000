06e547b9b5d43309e2c2d1055a4c4020fa00e58a
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-12 22:40:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,9 @@ def speak_to_grandma(input)
   if(input.match(/^.*[A-Z]$/) == nil)
     return "HUH?! SPEAK UP, SONNY!";
   elsif (input = "I LOVE YOU GRANDMA!")
-    puts "I LOVE YOU TOO PUMPKIN!";
+    return "I LOVE YOU TOO PUMPKIN!";
   else
-    puts "NO, NOT SINCE 1938!";
+    return "NO, NOT SINCE 1938!";
   end
 end
 
